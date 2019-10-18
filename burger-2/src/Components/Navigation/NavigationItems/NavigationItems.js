4af546290773e7f62ab7c73cbf3b4ffabd03a3e9
Justin Lamb <lambjg63@gmail.com>
2019-10-18 12:19:01 -0400

    backdrop

--- burger-2/src/Components/Navigation/NavigationItems/NavigationItems.js
+++ burger-2/src/Components/Navigation/NavigationItems/NavigationItems.js
@@ -3,7 +3,7 @@ import classes from "./NavigationItems.module.css"
 import NavigationItem from "./NavigationItem/NavigationItem"
 
 const navigationItems = (props) => (
-    <ul className={classes.navigationItems}>
+    <ul className={classes.NavigationItems}>
         <NavigationItem link="/" active>Burger Builder</NavigationItem>
         <NavigationItem linl="/">Checkout</NavigationItem>
     </ul>
